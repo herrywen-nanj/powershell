ecc735e7254a068d1562f62b00e6d6601c8f8c94
herrywen <herrywen@herrywendeAir.lan>
2019-01-06 16:48:52 +0800

    first commit

---
+++ 无标题3.ps1
@@ -0,0 +1,14 @@
+﻿#作用：powershell非交互式远程执行脚本，目标机器herrywen,herrywen，远程ip：192.168.255.135
+#远程机器开启Enable-PSRemoting –Force
+#远程机器设置访问列表Set-Item wsman:\localhost\client\trustedhosts *
+#                    Restart-Service WinRM
+$username = "herrywen"
+$password = "herrywen"
+$pass = ConvertTo-SecureString -AsPlainText $Password -Force
+$Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$pass
+#设置超时时间3600毫秒，也就是10分钟，默认三分钟，看具体项目，参考：https://docs.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/new-pssessionoption?view=powershell-5.0
+$timeout = New-PSSessionOption -OperationTimeout 3600
+$session_many = new-pssession -computername 192.168.255.135 -Credential $Cred -SessionOption $timeout
+#-ScriptBlock {}是执行一段命令，-Filepath是指定脚本
+#Invoke-Command -ComputerName 192.168.255.135 -Credential $Cred  -FilePath .\无标题4.ps1 -SessionOption 
+Invoke-Command -Session $session_many  -FilePath .\无标题4.ps1
---
+++ 无标题5.ps1
@@ -0,0 +1,17 @@
+﻿function delivery($msg)
+{
+$uri="https://oapi.dingtalk.com/robot/send?access_token=420ba1861fa435046c04d87fc1770d0a96b7fe5efa1b989a8e059982a0723764"
+$bodys=
+@"
+{"msgtype": "text","text": {"content": "$msg"}}
+"@
+Invoke-WebRequest -Uri $uri -ContentType "application/json;charset=utf-8" -Method POST  -Body $bodys
+}
+$null= Invoke-WebRequest -Uri http://www.baidu.com
+if ($? -eq 0){
+delivery "The website is not ok!"
+exit 1
+}
+else {
+delivery "The website is ok"
+}
