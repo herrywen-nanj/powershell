c6c3b91dbd19b92a9d9f9295e94939d4882db808
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2019-01-06 22:44:43 +0800

    Update jenkins-to-dingding.ps1

--- jenkins-to-dingding.ps1
+++ jenkins-to-dingding.ps1
@@ -1,4 +1,4 @@
-#作用：钉钉机器人告警
+#作用：钉钉机器人告警，@"  "@为多行命令写法http://blog.51cto.com/yoke88/2069906
 function delivery($msg)
 {
 $uri="https://oapi.dingtalk.com/robot/send?access_token=420ba1861fa435046c04d87fc1770d0a96b7fe5efa1b989a8e059982a0723764"
