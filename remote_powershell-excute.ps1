e14a6a670eb01c5ccc11923fbbfd885ca5510b47
herrywen-nanj <33476935+herrywen-nanj@users.noreply.github.com>
2019-01-23 12:55:53 +0800

    Update remote_powershell-excute.ps1

--- remote_powershell-excute.ps1
+++ remote_powershell-excute.ps1
@@ -6,7 +6,7 @@ $username = "herrywen"
 $password = "herrywen"
 $pass = ConvertTo-SecureString -AsPlainText $Password -Force
 $Cred = New-Object System.Management.Automation.PSCredential -ArgumentList $username,$pass
-#设置超时时间3600毫秒，也就是10分钟，默认三分钟，看具体项目，参考：https://docs.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/new-pssessionoption?view=powershell-5.0
+#设置超时时间3600毫秒，也就是3.6秒，默认三分钟，看具体项目，参考：https://docs.microsoft.com/zh-cn/powershell/module/microsoft.powershell.core/new-pssessionoption?view=powershell-5.0
 $timeout = New-PSSessionOption -OperationTimeout 3600
 $session_many = new-pssession -computername 192.168.255.135 -Credential $Cred -SessionOption $timeout
 #-ScriptBlock {}是执行一段命令，-Filepath是指定脚本
